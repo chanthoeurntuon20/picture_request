7021ebffcd97a28697cd800d66ddfd8aeb5f8ce5
chanthoeurntuon20 <54494449+chanthoeurntuon20@users.noreply.github.com>
2019-11-20 20:30:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var url = "https://pixabay.com/api/?key=14001068-da63091f2a2cb98e1d7cc1d82&q=yellow+flowers&image_type=photo&pretty=true";
     var result = "";
     $.getJSON(url,function(data){
-       data.forEach(element => {
+       data.hits.forEach(element => {
            const {name,email,phone,website} = element;
            if(element.id <= 5){
             result += `
